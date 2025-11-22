574ec4dad3fea156bd38dac6fb78511f81e67c78
Danish Mustafa <danishjajja86@gmail.com>
2025-11-22 15:37:44 +0500

    python roadmap

---
+++ 01_python/01_python_basics/09_basic_errors.py
@@ -0,0 +1,24 @@
+# Syntax errors are caused by mistakes in the code that the interpreter cannot understand.
+
+# print("Hello, World!") # This will cause a syntax error.
+# Missing parentheses is a syntax error.
+# print("Hello, World!") # This will cause a syntax error.
+
+# IndentationError is a syntax error.
+# print("Hello, World!") # This will cause a syntax error.
+# print("Hello, World!") # This will cause a syntax error.
+# print("Hello, World!") # This will cause a syntax error.
+
+
+
+
+# Runtime errors are caused by mistakes in the code that the interpreter can understand but will cause the program to crash.
+
+# Dividing by zero is a runtime error.
+# print(10 / 0) # This will cause a runtime error.
+
+# TypeError is a runtime error.
+# print(10 + "10") # This will cause a runtime error.
+
+# ValueError is a runtime error.
+# print(int("10a")) # This will cause a runtime error.
